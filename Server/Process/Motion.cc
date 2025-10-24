525c70242f2e82b17ae3b04a24767828a419eda4
RuinedLiberty <59511480+RuinedLiberty@users.noreply.github.com>
2025-10-24 11:14:28 -0400

    Clean

--- Server/Process/Motion.cc
+++ Server/Process/Motion.cc
@@ -36,16 +36,13 @@ void tick_entity_motion(Simulation *sim, Entity &ent) {
         }
     }
 
-    // Apply acceleration (after scaling by speed ratio)
     ent.velocity += (ent.acceleration * ent.speed_ratio);
 
-    // Integrate position and collision response
     ent.set_x(ent.get_x() + ent.velocity.x + ent.collision_velocity.x);
     ent.set_y(ent.get_y() + ent.velocity.y + ent.collision_velocity.y);
     ent.collision_velocity *= 0.5;
     ent.velocity += ent.collision_velocity;
 
-    // Keep non-projectiles within arena bounds
     if (!ent.has_component(kPetal) && !ent.has_component(kWeb)) {
         ent.set_x(fclamp(ent.get_x(), ent.get_radius(), ARENA_WIDTH - ent.get_radius()));
         ent.set_y(fclamp(ent.get_y(), ent.get_radius(), ARENA_HEIGHT - ent.get_radius()));
